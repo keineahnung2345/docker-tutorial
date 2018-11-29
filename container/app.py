2218e9a1b4878ca01b1e490fc0d947522dd9a800
keineahnung2345 <mimifasosofamire1123@gmail.com>
2018-11-29 15:37:54 +0800

    Add docker container tutorial

---
+++ container/app.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
+#--------------------- 
+#作者：无始之名 
+#来源：CSDN 
+#原文：https://blog.csdn.net/u011499747/article/details/73693384 
+#版权声明：本文为博主原创文章，转载请附上博文链接！
